907270bf49143a26c2d9642773babbfa793d55d9
felix miranda <felixmiranda.net@gmail.com>
2017-03-16 20:13:59 -0500

    Estructura del proyecto
    Se crea la estructura del proyecto basado en DDD

---
+++ ProyectoModeloDDD.Domain/Entities/Cliente.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoModeloDDD.Domain.Entities
+{
+    public class Cliente
+    {
+        public int ClienteId { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Email { get; set; }
+        public DateTime FechaRegistro { get; set; }
+        public bool Activo { get; set; }
+
+
+        public bool ClienteEspecial(Cliente cliente)
+        {
+            return cliente.Activo && DateTime.Now.Year - cliente.FechaRegistro.Year >= 5;
+        }
+
+    }
+}
---
+++ ProyectoModeloDDD.Infra.Data/Contexto/ProyectoModeloContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProyectoModeloDDD.Domain.Entities;
+
+namespace ProyectoModeloDDD.Infra.Data.Contexto
+{
+    public class ProyectoModeloContext : DbContext
+    {
+        public ProyectoModeloContext()
+            : base("ProyectoModeloDDD")
+        {
+            
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+
+    }
+}
